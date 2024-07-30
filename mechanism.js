69a9f3387774732e3d2126b3b906a3e8908f09e8
shw9601 <shw9601@rit.edu>
2024-07-30 03:02:48 -0400

    Add equation functionality
    Add equation function and logic to support typing in digits and symbols.
    Add mouseover effect and event listeners for easier visual interactivity.

--- mechanism.js
+++ mechanism.js
@@ -1,3 +1,5 @@
+const digits = "1234567890";
+
 
 function add(first, second) {
     return parseInt(first) + parseInt(second);
@@ -32,4 +34,51 @@ function operate(first, operator, second) {
     }
 
     console.log(result);
+}
+
+
+
+
+const buttons = document.querySelectorAll(".button");
+buttons.forEach(button => button.addEventListener("mouseover", () => {
+    button.style.backgroundColor = "green";
+}));
+buttons.forEach(button => button.addEventListener("mouseout", () => {
+    button.style.backgroundColor = "darkGrey";
+}));
+
+const expression = {
+    first: "0",
+    operator: "",
+    second: ""
+};
+
+
+function updateDisplay() {
+    let displayString = expression.first + " " + expression.operator + " " + expression.second;
+    let display = document.querySelector("#display");
+    display.textContent = displayString;
+}
+
+updateDisplay();
+
+function addToExpression(newChar) {
+    if (digits.includes(newChar)) {
+        if (expression.operator === "") {
+            if (expression.first === "0") {
+                expression.first = newChar;
+            }
+            else {
+                expression.first = expression.first + newChar;
+            }
+        }
+        else {
+            expression.second = expression.second + newChar;
+        }
+    }
+    else {
+        expression.operator = newChar;
+    }
+
+    updateDisplay();
 }
