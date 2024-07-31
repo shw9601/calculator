b70fcfad7f6f95c2235ef937bbbcaf913437ae06
shw9601 <shw9601@rit.edu>
2024-07-30 23:56:06 -0400

    Final commit - first release
    Add all functionality for buttons. Finish formatting concerns for
    display.

--- mechanism.js
+++ mechanism.js
@@ -33,7 +33,7 @@ function operate(first, operator, second) {
         result = divide(first, second);
     }
 
-    console.log(result);
+    return result;
 }
 
 
@@ -82,3 +82,79 @@ function addToExpression(newChar) {
 
     updateDisplay();
 }
+
+const zero = document.querySelector("#zero");
+zero.addEventListener("click", () => {
+    addToExpression("0");
+})
+const one = document.querySelector("#one");
+one.addEventListener("click", () => {
+    addToExpression("1");
+});
+const two = document.querySelector("#two");
+two.addEventListener("click", () => {
+    addToExpression("2");
+});
+const three = document.querySelector("#three");
+three.addEventListener("click", () => {
+    addToExpression("3");
+});
+const four = document.querySelector("#four");
+four.addEventListener("click", () => {
+    addToExpression("4");
+});
+const five = document.querySelector("#five");
+five.addEventListener("click", () => {
+    addToExpression("5");
+});
+const six = document.querySelector("#six");
+six.addEventListener("click", () => {
+    addToExpression("6");
+});
+const seven = document.querySelector("#seven");
+seven.addEventListener("click", () => {
+    addToExpression("7");
+});
+const eight = document.querySelector("#eight");
+eight.addEventListener("click", () => {
+    addToExpression("8");
+});
+const nine = document.querySelector("#nine");
+nine.addEventListener("click", () => {
+    addToExpression("9");
+});
+
+
+
+const AC = document.querySelector("#AC");
+AC.addEventListener("click", () => {
+    expression.first = "0";
+    expression.operator = "";
+    expression.second = "";
+    updateDisplay();
+});
+
+const plus = document.querySelector("#plus");
+plus.addEventListener("click", () => {
+    addToExpression("+");
+});
+const minus = document.querySelector("#minus");
+minus.addEventListener("click", () => {
+    addToExpression("-");
+});
+const times = document.querySelector("#multiply");
+times.addEventListener("click", () => {
+    addToExpression("*");
+});
+const divided = document.querySelector("#divide");
+divided.addEventListener("click", () => {
+    addToExpression("/");
+});
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", () => {
+    expression.first = operate(expression.first, expression.operator, expression.second);
+    expression.operator = "";
+    expression.second = "";
+    updateDisplay();
+});
